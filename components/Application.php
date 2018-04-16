f316e533cfe35f9d4452d9d8f258ae6d09292632
Johnn3volta <johnn3volta@gmail.com>
2018-04-16 17:46:24 +0300

    New files

---
+++ components/Application.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ContentManager5
+ * Date: 16.04.2018
+ * Time: 16:15
+ */
+
+namespace app\components;
+
+
+class Application{
+
+}
---
+++ funcs.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ContentManager5
+ * Date: 16.04.2018
+ * Time: 16:05
+ */
